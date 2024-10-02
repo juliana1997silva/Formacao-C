30802338855f7c8b24c370416fbf346756526d06
Juliana <julianajesus06@hotmail.com>
2024-10-02 02:25:15 -0300

    manipulando arrays

---
+++ forca/forca.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char palavrasecreta[20];
+
+    sprintf(palavrasecreta, "MELANCIA");
+
+    int acertou = 0;
+    int enforcou = 0;
+    
+    do
+    {
+        for (int i = 0; i < strlen(palavrasecreta); i++)
+        {
+            printf("_ ");
+        }
+        printf("\n");
+
+        char chute;
+        scanf(" %c", &chute);
+
+        } while (!acertou && !enforcou);
+}
